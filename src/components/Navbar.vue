f3531682866073877f5350c4dc2ff0158aae1e22
Artur8800 <artursviridonov@gmail.com>
2020-04-14 22:33:50 +0300

    Navbar added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="deep-purple darken-4">
+    <div class="nav-wrapper">
+      <a id="sideTrigger" href="#" data-target="slide-out" class="sidenav-trigger">
+        <i class="material-icons">menu</i>
+      </a>
+
+      <div class="nav-user valign-wrapper right">
+        <span class="user_name white-text">Username</span>
+
+        <img src="img/technik.jpg" alt class="user_photo circle" />
+        <!-- notice the "circle" class -->
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style>
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,20 +1,7 @@
 <template>
   <div class="main-layout">
     <header>
-      <nav class="deep-purple darken-4">
-        <div class="nav-wrapper">
-          <a id="sideTrigger" href="#" data-target="slide-out" class="sidenav-trigger">
-            <i class="material-icons">menu</i>
-          </a>
-
-          <div class="nav-user valign-wrapper right">
-            <span class="user_name white-text">Username</span>
-
-            <img src="img/technik.jpg" alt class="user_photo circle" />
-            <!-- notice the "circle" class -->
-          </div>
-        </div>
-      </nav>
+      <Navbar />
       <ul id="slide-out" class="sidenav">
         <li>
           <div class="user-view">
@@ -57,3 +44,12 @@
     </footer>
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
